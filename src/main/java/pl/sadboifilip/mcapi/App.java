6b67ae6aa73ecdd8b3804470e210b053edb78102
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2021-07-07 02:25:10 +0200

    base command

--- src/main/java/pl/sadboifilip/mcapi/App.java
+++ src/main/java/pl/sadboifilip/mcapi/App.java
@@ -2,6 +2,8 @@ package pl.sadboifilip.mcapi;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import pl.sadboifilip.mcapi.commands.ApiCredentialsCommand;
+
 /**
  * Hello world!
  *
@@ -9,6 +11,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class App extends JavaPlugin {
     @Override
     public void onEnable() {
+        getServer().getConsoleSender().sendMessage("REST API ENABLED!");
         getLogger().info("REST API ENABLED!");
+
+        this.getCommand("apitoken").setExecutor(new ApiCredentialsCommand());
     }
 }
---
+++ src/main/java/pl/sadboifilip/mcapi/commands/ApiCredentialsCommand.java
@@ -0,0 +1,20 @@
+package pl.sadboifilip.mcapi.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class ApiCredentialsCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            player.sendMessage("Hi");
+            // Here we need to give items to our player
+        }
+        return true;
+    }
+
+}
